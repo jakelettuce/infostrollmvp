3e44c9514a441ebd3b882ff3d3c5df2fd64eaf6a
Jake <freakyjake@gmail.com>
2025-06-03 17:36:43 -0400

    switched order around

--- src/config/sites.ts
+++ src/config/sites.ts
@@ -5,10 +5,10 @@ export interface SiteConfig {
 }
 
 export const sites: SiteConfig[] = [
-  { name: 'Way to Go', url: 'http://a-way-to-go.com/', unlocked: true },
+  { name: 'Stimulation Clicker', url: 'https://neal.fun/stimulation-clicker', unlocked: false },
   { name: 'Lettuce Labs', url: 'https://www.jakeladdis.com/', unlocked: false },
   { name: 'Patatap', url: 'https://patatap.com/', unlocked: false },
-  { name: 'Stimulation Clicker', url: 'https://neal.fun/stimulation-clicker', unlocked: false },
+  { name: 'Way to Go', url: 'http://a-way-to-go.com/', unlocked: true },
   { name: 'Superbad', url: 'https://www.superbad.com/', unlocked: false },
   { name: 'WNDR', url: 'https://www.sandrinewij.be/WNDR/', unlocked: false },
   { name: 'ECSpace', url: 'https://www.erdong-chen.com/', unlocked: false },
