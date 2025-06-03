f7d5dedc9157f79c511b878be20fb10b475dd4bb
Jake <freakyjake@gmail.com>
2025-06-03 16:45:00 -0400

    im pretty happy with this, now on to some aesthetics; added my site

--- src/config/sites.ts
+++ src/config/sites.ts
@@ -6,6 +6,7 @@ export interface SiteConfig {
 
 export const sites: SiteConfig[] = [
   { name: 'Way to Go', url: 'http://a-way-to-go.com/', unlocked: false },
+  { name: 'Lettuce Labs', url: 'https://www.jakeladdis.com/', unlocked: false },
   { name: 'Patatap', url: 'https://patatap.com/', unlocked: true },
   { name: 'Stimulation Clicker', url: 'https://neal.fun/stimulation-clicker', unlocked: false },
   { name: 'Superbad', url: 'https://www.superbad.com/', unlocked: false },
